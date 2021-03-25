ce3b4e48c4d1570097ce85276605b9264fc8abce
unknown <dodjiakakpo02@gmail.com>
2021-03-25 11:56:18 +0100

    2ene commit

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ app.use((req,res,next)=>{
     res.setHeader('Access-Control-Allow-Origin','*')
     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE')
     next()
-
 })
 app.use(express.urlencoded({extended:true}))
 app.use(express.json({extended:true}))
